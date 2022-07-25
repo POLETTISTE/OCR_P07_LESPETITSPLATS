1ca138a0457397bcfa3af87e6160d9263bd8d4fb
STEPHANE POLETTI <polettistephane@msn.com>
2022-07-25 16:34:59 +0200

    filter ingredient ok

--- scripts/pages/search.js
+++ scripts/pages/search.js
@@ -1,5 +1,4 @@
  function searchInputBar() {
-    let essai = [];
 
     //MOINS DE 3 CARACTERES
   if (inputForm1.value.length < 3) {
@@ -19,30 +18,15 @@
     
     if (filteredRecipes.length === 0) {
 
-
-       essai = recipes.filter(element => {
-        element.ingredients.filter(x => {
-          console.log(x.ingredient);
-        })
-      })
-      console.log(essai)
         // si on a aucune donnee filtree, on prend recipes
-
-
+        
+        resultIng = (item) => item.ingredient.toLowerCase().includes(actualValueInput.toLowerCase());
+        
         result = recipes.filter(recipe => {
-          console.log(recipe.name)
-          console.log(recipe.description)
-
-
-
         return recipe.name.toLowerCase().includes(actualValueInput.toLowerCase()) || 
-               recipe.description.toLowerCase().includes(actualValueInput.toLowerCase()) 
-              //  ||
-              //  tempIng.includes(actualValueInput.toLowerCase())
+               recipe.description.toLowerCase().includes(actualValueInput.toLowerCase()) ||
+               recipe.ingredients.some(resultIng);
 
-        // recipe.ingredients.filter(ele => {
-        //   return ele.ingredient.toLowerCase().includes(actualValueInput.toLowerCase())
-        // })
       })
       // si on a deja des donnes filtrees dans recipearray, alors on prend filteredRecipes pour filter
     }else {
